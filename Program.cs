eaa8753d787e971a3d99ed8610cff955b8c16098
Danilo Costa <dandan_costa2222@outlook.com>
2025-03-16 21:31:45 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+double A = 0, B = 0;
+
+Console.Write("Insira o valor A: ");
+A = Double.Parse(Console.ReadLine());
+
+Console.Write("Insira o valor B: ");
+B = Double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Resultado da soma: {A + B}");
+Console.WriteLine($"Resultado da subtração: {A - B}");
+
+if (B == 0){
+   Console.WriteLine("Resultado da divisão: indeterminado(±∞)"); 
+}
+else{
+    Console.WriteLine($"Resultado da divisão: {A/B}"); 
+}
+
+Console.WriteLine($"Resultado da multiplicação: {A * B}");
+
+
